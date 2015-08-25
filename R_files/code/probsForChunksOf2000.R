0d911aee519b5a3a6fd1095d6bdc4dc0745311a0
Bob Gorman <rgorman1@unl.edu>
2015-08-25 11:23:04 -0500

    Figures probabilities for sample function.
    Script figuring probailties of 2000 word chunks so that each
    has equal chance of being sampled. Reduces frequency of Iliad
    and Odyssey. The data are in parametersWithProbs,csv, the script is
    probsForChunksOf2000.R.

---
+++ R_files/code/probsForChunksOf2000.R
@@ -0,0 +1,47 @@
+# this scipt was used to figure probabilities to equalize sampling when chunk size was set to 200
+
+
+
+# variable for number of authors
+x <- 1/13
+
+
+#aeschylus
+(aeschy <- x/25)
+
+# athen
+(athen <- x/22)
+
+# diodorus
+(diod <- x/13)
+
+#herodotus
+(hdt <- x/16)
+
+# hesiod
+(hesiod <- x /10)
+
+# iliad
+(iliad <- x/74)
+
+# lysias
+(lysias <- x/6)
+
+# odyssey
+(odyssey <- x/44)
+
+# plato
+(plato <- x/3)
+
+# plutarch
+(plut <- x/11)
+
+# polybius
+(plyb <- x/14)
+
+# sophocles
+(soph <- x/27)
+
+# thucydides
+(thuc <- x/12)
+
