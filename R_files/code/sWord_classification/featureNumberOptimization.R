2b66596c24c93ca25718228b8d3c0b0c76639f45
Bob Gorman <rgorman1@unl.edu>
2015-08-31 23:32:10 -0500

    Minor changes to .R script.

--- R_files/code/sWord_classification/featureNumberOptimization.R
+++ R_files/code/sWord_classification/featureNumberOptimization.R
@@ -2,6 +2,10 @@
 # This is a script to test what number of features is the most effective when using the 
 # Naive Bayes classification
 
-
+# sort smaller.df by column means
 sorted.df <- smaller.df[, order(colMeans(smaller.df), decreasing=TRUE) ]
-write.csv (smaller.df, file="Rresults/Naive_Bayes_predictions/toSort.csv")
+
+
+
+View(smaller.df)
+View(sorted.df)
