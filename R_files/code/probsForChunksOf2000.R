e977acc912539a1e6072845acb201e2343b64797
Bob Gorman <rgorman1@unl.edu>
2015-09-20 16:06:12 -0500

    Files to cull short chunks for test of Naive Bayes.

--- R_files/code/probsForChunksOf2000.R
+++ R_files/code/probsForChunksOf2000.R
@@ -8,40 +8,51 @@ x <- 1/11
 
 #aeschylus
 (aeschy <- x/25)
+(aeschy <- x/16)
 
 # athen
 (athen <- x/22)
+(athen <- x/21)
 
 # diodorus
 (diod <- x/13)
+(diod <- x/12)
 
 #herodotus
 (hdt <- x/16)
+(hdt <- x/15)
 
 # hesiod
 (hesiod <- x /10)
+(hesiod <- x /7)
 
 # iliad
 (iliad <- x/62)
+(iliad <- x/61)
 
 # lysias
 (lysias <- x/6*.5 )
 
 # odyssey
 (odyssey <- x/50)
+(odyssey <- x/49)
 
 # plato
 (plato <- x/3)
 
 # plutarch
 (plut <- x/11)
+(plut <- x/9)
 
 # polybius
 (plyb <- x/14)
+(plyb <- x/13)
 
 # sophocles
 (soph <- x/27)
+(soph <- x/20)
 
 # thucydides
 (thuc <- x/12)
+(thuc <- x/11)
 
