c8db23694348b38b9547f7c23cdd48e2d8cefb68
Bob Gorman <rgorman1@unl.edu>
2015-09-26 10:57:11 -0500

    Adjusts rel_pos_iterationTest_sept25.csv for plot().

--- R_files/code/sWord_classification/extractIterationResults.R
+++ R_files/code/sWord_classification/extractIterationResults.R
@@ -2,25 +2,25 @@ list1 <- load(file="Rresults/iteration_list_sept13.R")
 list2 <- load(file="Rresults/iteration_list2_sept13.R")
 summary(list1)
 list1
-total.errors.l[[1]]
+total.errors.l[[2]]
 total.errors2.l
 total.errors2.l[[1]]
 length(total.errors2.l[[1]])
-sum(total.errors2.l[[1]])/2800
+sum(total.errors2.l[[1]])/2400
 unlist(total.errors2.l[[1]])
 lapply(total.errors2.l, sum)
 unlist(lapply(total.errors2.l, sum))
 unlist(lapply(total.errors2.l, sum))/2800
-1-(unlist(lapply(total.errors2.l, sum))/2800)
+1-(unlist(lapply(total.errors2.l, sum))/2400)
 
 x <- seq(2, 500, 2)
 iteration.m <- matrix (x, nrow=250, ncol=1)
 iteration.m <- (cbind(iteration.m, lapply(total.errors2.l, sum)))
-iteration.m <- (cbind(iteration.m, unlist(lapply(total.errors2.l, sum))/2800 ))
-iteration.m <- (cbind(iteration.m, 1-(unlist(lapply(total.errors2.l, sum))/2800) ))
+iteration.m <- (cbind(iteration.m, unlist(lapply(total.errors2.l, sum))/2400 ))
+iteration.m <- (cbind(iteration.m, 1-(unlist(lapply(total.errors2.l, sum))/2400) ))
 
 
 
 
 View(iteration.m)
-write.csv(iteration.m, file="Rresults/rel_pos_iterationTest_sept13.csv")
+write.csv(iteration.m, file="Rresults/rel_pos_iterationTest_sept25.csv")
