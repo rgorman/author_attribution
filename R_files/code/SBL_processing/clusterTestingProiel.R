bd9eda2141e89aff311e247b812805d1ec5fc757
Bob Gorman <rgorman1@unl.edu>
2015-11-11 18:32:20 -0600

    Removes Jude and 3 John from clustering data.

--- R_files/code/SBL_processing/clusterTestingProiel.R
+++ R_files/code/SBL_processing/clusterTestingProiel.R
@@ -1,7 +1,8 @@
 library(XML)
+library(tm)
 library(stylo)
 source("code/corpusFunctions.R")
-input.dir <- "data/relationFiles"
+input.dir <- "../proiel/proiel_output"
 files.v <- dir(path=input.dir, pattern=".*xml")
 
 
@@ -15,33 +16,8 @@ for (i in 1:length(files.v)) {
 
 
 
-#the following script collects the data for testing
-#when the test data are full files use the immediately following sequence
-
-input.dir2 <- "data/relationFiles/testFiles"
-test.files.v <- dir (path=input.dir2, pattern=".*xml")
-
-for (i in 1:length(test.files.v)) {
-  doc.object <- xmlTreeParse(file.path(input.dir2, test.files.v[i]), useInternalNodes=TRUE)
-  sword.data <-getSwordNgramTableList(doc.object)
-  book.freqs.l[[test.files.v[i]]] <-sword.data
-  
-}
 
 
-#when the test data are partial files, use the immediately following sequence
-
-#the following script calls the user-defined function "getSwordChunkMaster).
-#this function will return a list of lists of tables, each table with a maximum of words = the second variable
-
-
-for(i in 1:length(test.files.v)){
-  doc.object <- xmlTreeParse(file.path(input.dir2, test.files.v[i]), useInternalNodes=TRUE)
-  chunk.data.l <- getSwordChunkMaster(doc.object, 2500)
-  s <- sample(chunk.data.l, 1)
-  book.freqs.l[[test.files.v[i]]] <-s
-  
-}
 
 
 
@@ -58,18 +34,36 @@ result <- xtabs(Freq ~ ID+sword.ngrams, data=freqs.df)
 #convert wide format table to matrix object
 final.m <- apply(result, 2, as.numeric)
 
-file.names <- append(files.v, test.files.v, after=length(files.v))
+dim(final.m)
+
+# make names for rows
+names_for_files.v <- gsub (".xml", "", files.v)
+
 
 #substitute files names for row numbers
-rownames(final.m) <-file.names
+rownames(final.m) <-names_for_files.v
 
-rownames(final.m) <-files.v
+rownames(final.m)
+
+View(final.m)
+dim(final.m)
 
 #reduce data matrix to features with largest means (most common features)
-smaller.m <- final.m[, apply(final.m,2,mean)>=.05]
+smaller.m <- final.m[, apply(final.m,2,mean)>=.005]
 
 #check size of reduced feature set
 dim(smaller.m)
+
+
+#create distance object as input to hclust algorithm
+dist.smaller.m <- dist(smaller.m)
+
+
+# run hclust and get result in object (available methods are "ward.D", "ward.D2", "single", "complete" "average", "mcquitty", "median", or "centroid")
+groups <- hclust(dist.smaller.m, method="complete")
+
+# plot the results
+plot(groups, hang=-1)
 
 #process data through stylo
 stylo.results <-stylo(frequencies = smaller.m)
